278024edc4b715bb3077e873eb4752a1a5896a4c
Flávio Couto <flavio.couto@live.com>
2025-04-22 15:45:49 -0300

    TASK Add ClienteDtoMapCliente

--- PROJETO_2025_DDD/Camada_Application/Mapping/MapperApp.cs
+++ PROJETO_2025_DDD/Camada_Application/Mapping/MapperApp.cs
@@ -5,6 +5,8 @@ namespace Camada_Application.Mapping
 {
     public static class MapperApp
     {
+        #region ClienteMapClienteDto
+
         public static void ClienteMapClienteDto(List<Cliente> cliente, out List<ClienteDto> clienteDto)
         {
             if (cliente == null) throw new ArgumentNullException(nameof(cliente));
@@ -24,5 +26,26 @@ namespace Camada_Application.Mapping
                 Nome = cliente.Nome
             };
         }
+
+        #endregion
+
+        #region ClienteDtoMapCliente
+
+        public static void ClienteDtoMapCliente(List<ClienteDto> clienteDto, out List<Cliente> cliente)
+        {
+            if (clienteDto == null) throw new ArgumentNullException(nameof(clienteDto));
+            cliente = clienteDto.Select(dto => new Cliente(
+                dto.Nome, 
+                dto.ClienteId
+                )).ToList();
+        }
+
+        public static void ClienteDtoMapCliente(ClienteDto clienteDto, out Cliente cliente)
+        {
+            if (clienteDto == null) throw new ArgumentNullException(nameof(clienteDto));
+            cliente = new Cliente(clienteDto.Nome, clienteDto.ClienteId);
+        }
+
+        #endregion
     }
 }
