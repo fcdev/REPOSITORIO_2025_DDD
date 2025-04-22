3f03c04c786f0d5281b4708f380ecc10f72b463d
Flávio Couto <flavio.couto@live.com>
2025-04-22 15:38:07 -0300

    TASK removendo usings

--- PROJETO_2025_DDD/Camada_Application/Services/ClienteApp.cs
+++ PROJETO_2025_DDD/Camada_Application/Services/ClienteApp.cs
@@ -3,11 +3,6 @@ using Camada_Application.Interfaces;
 using Camada_Application.Mapping;
 using Camada_Domain.Entities;
 using Camada_Domain.Interfaces.IServices;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Camada_Application.Services
 {
